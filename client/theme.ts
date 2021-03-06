977dfab934fdac0a2b73278c544d834cd97facdd
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-03-06 16:37:09 +0100

    Set up MaterialUI

---
+++ client/theme.ts
@@ -0,0 +1,16 @@
+import { brown, green } from "@material-ui/core/colors"
+import { createMuiTheme } from "@material-ui/core/styles"
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: green[300],
+      main: green[800],
+    },
+    secondary: {
+      main: brown[400],
+    },
+  },
+})
+
+export default theme
---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import { CssBaseline, ThemeProvider } from "@material-ui/core"
+import { AppProps } from "next/dist/next-server/lib/router/router"
+import Head from "next/head"
+import theme from "../client/theme"
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title> Los Guindales - Documentos </title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default MyApp
