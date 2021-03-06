b588272387e9da5a1f91f155d8312e0f5ffa9941
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-03-06 16:49:53 +0100

    Remove initial unnecessary files

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Typography } from "@material-ui/core"
+
+export default function Home() {
+  return (
+    <Container>
+      <Typography variant="h4" component="h1">
+        Salvoconducto para el taller del 27-28 de marzo
+      </Typography>
+    </Container>
+  )
+}
