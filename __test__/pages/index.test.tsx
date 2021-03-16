ed5838e0b8cf1780b97377c9eb9284f309b93105
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-03-16 15:30:41 +0100

    Refactor code

--- __test__/pages/index.test.tsx
+++ __test__/pages/index.test.tsx
@@ -1,32 +1,35 @@
 import Home, { event } from "pages"
-import { render } from "utils/testUtils"
+import { render, screen } from "utils/testUtils"
 import { formatDate } from "utils/utils"
 
 describe("index", () => {
+  beforeEach(() => {
+    render(<Home />)
+  })
   describe("page title", () => {
     it("should have a title", () => {
-      const { getByText } = render(<Home />)
-
-      const titleElement = getByText(/Salvoconducto/, { selector: "h1" })
+      const titleElement = screen.getByText("Salvoconducto", {
+        selector: "h1",
+        exact: false,
+      })
 
       expect(titleElement).toBeDefined()
     })
     it("should display the event name", () => {
-      const { getByText } = render(<Home />)
-
-      const eventNameElement = getByText(new RegExp(event.name), {
+      const eventNameElement = screen.getByText(event.name, {
         selector: "h1 ",
+        exact: false,
       })
 
       expect(eventNameElement).toBeDefined()
     })
     it("should display the event dates", () => {
-      const { getByText } = render(<Home />)
-
-      const startDateElement = getByText(
-        new RegExp(formatDate(event.startDate))
-      )
-      const endDateElement = getByText(new RegExp(formatDate(event.endDate)))
+      const startDateElement = screen.getByText(formatDate(event.startDate), {
+        exact: false,
+      })
+      const endDateElement = screen.getByText(formatDate(event.endDate), {
+        exact: false,
+      })
 
       expect(startDateElement).toBeDefined()
       expect(endDateElement).toBeDefined()
