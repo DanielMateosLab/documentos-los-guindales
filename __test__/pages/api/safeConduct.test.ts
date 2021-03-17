9ec820152ef2135914f84b7a74b5576cfe21e878
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-03-17 08:51:50 +0100

    Allow only POST method

--- __test__/pages/api/safeConduct.test.ts
+++ __test__/pages/api/safeConduct.test.ts
@@ -1,5 +1,6 @@
-import { createMocks } from "node-mocks-http"
+import { createMocks, RequestOptions } from "node-mocks-http"
 import safeConductHandler from "pages/api/safeConduct"
+import { MethodNotAllowedError } from "utils/errors"
 
 jest.mock("html-pdf", () => ({
   create: () => ({
@@ -10,11 +11,33 @@ const mockToBuffer = jest.fn()
 
 describe("/api/safeConduct", () => {
   describe("POST", () => {
+    it("should throw a MethodNotAllowedError with get, put, patch or del", async () => {
+      const notAllowedMethods: RequestOptions["method"][] = [
+        "GET",
+        "PUT",
+        "PATCH",
+        "DELETE",
+      ]
+
+      expect.assertions(notAllowedMethods.length)
+
+      for (let method in notAllowedMethods) {
+        const { req, res } = createMocks({
+          method: method as RequestOptions["method"],
+        })
+
+        try {
+          await safeConductHandler(req, res)
+        } catch (e) {
+          expect(e).toBeInstanceOf(MethodNotAllowedError)
+        }
+      }
+    })
     it("should throw if the buffer is an error", () => {
       const error = "mockBufferError"
       mockToBuffer.mockImplementationOnce((cb) => cb(error))
 
-      const { req, res } = createMocks()
+      const { req, res } = createMocks({ method: "POST" })
 
       expect(() => safeConductHandler(req, res)).toThrowError(error)
     })
--- pages/api/safeConduct.tsx
+++ pages/api/safeConduct.tsx
@@ -2,12 +2,16 @@ import SafeConduct from "client/components/SafeConduct"
 import pdf from "html-pdf"
 import { NextApiHandler } from "next"
 import { renderToStaticMarkup } from "react-dom/server"
+import { MethodNotAllowedError } from "utils/errors"
 import { SafeConductPostResponse } from "utils/types"
 
 const safeConductHandler: NextApiHandler<SafeConductPostResponse> = (
   req,
   res
 ) => {
+  //TODO: craete a function to encapsulate this logic and its tests and put it in your module
+  if (req.method !== "POST") throw new MethodNotAllowedError()
+
   const htmlSafeConduct = renderToStaticMarkup(
     <SafeConduct
       name={req.body.name}
