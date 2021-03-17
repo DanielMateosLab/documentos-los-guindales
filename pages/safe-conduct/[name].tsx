f3ec95feef2bf87e89a8f967af849a9d947b2bfa
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-03-17 16:42:44 +0100

    Clean code. Will try to use another strategy

--- pages/safe-conduct/[name].tsx
+++ pages/safe-conduct/[name].tsx
@@ -1,49 +1,14 @@
-import SafeConduct from "client/components/SafeConduct"
-import pdf from "html-pdf"
-import { GetServerSideProps } from "next"
-import { renderToStaticMarkup } from "react-dom/server"
+import { useRouter } from "next/router"
 import { parseQueryParameters } from "utils/utils"
 
-export const getServerSideProps: GetServerSideProps = async ({
-  req,
-  res,
-  query,
-}) => {
+export default function SafeConductPage() {
+  const { query } = useRouter()
   const user = {
     name: decodeURIComponent(parseQueryParameters(query.name)),
     identityDocument: decodeURIComponent(
       parseQueryParameters(query.identityDocument)
     ),
   }
-  const htmlSafeConduct = renderToStaticMarkup(
-    <SafeConduct name={user.name} identityDocument={user.identityDocument} />
-  )
 
-  pdf
-    .create(htmlSafeConduct, {
-      format: "A4",
-      orientation: "portrait",
-    })
-    .toBuffer((error, buffer) => {
-      if (error) {
-        return {
-          props: {
-            error,
-          },
-        }
-      }
-
-      res.end(buffer)
-    })
-  return {
-    props: {},
-  }
-}
-
-export default function SafeConductPage(props: any) {
-  return (
-    <div>
-      {props.error && <p>Ha ocurrido un error: {props.error.message}</p>}
-    </div>
-  )
+  return <div>Loading...</div>
 }
