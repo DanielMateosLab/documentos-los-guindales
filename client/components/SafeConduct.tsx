8ddb4e9a12b3f4443954f48e1305b5738e4de1a1
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-03-17 16:03:58 +0100

    Decrease top margin

--- client/components/SafeConduct.tsx
+++ client/components/SafeConduct.tsx
@@ -5,7 +5,7 @@ import { formatDate } from "utils/utils"
 
 const useStyles = makeStyles((theme) => ({
   title: {
-    marginTop: theme.spacing(4),
+    marginTop: theme.spacing(2),
     marginBottom: theme.spacing(4),
   },
   indented: {
