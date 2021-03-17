4dafd7f1687601417e39a48c618c2875f1b8ee00
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-03-17 11:41:37 +0100

    Update validation error messages

--- utils/validation.ts
+++ utils/validation.ts
@@ -1,7 +1,28 @@
 import * as yup from "yup"
 
+export const requiredErrorText = "Campo obligatorio"
+export const getMaxErrorText = (max: number) =>
+  `Debe tener ${max} o menos caracteres`
+export const getMinErrorText = (min: number) =>
+  `Debe tener ${min} o más caracteres`
+
+export const emailMaxCharacters = 256
+export const emailErrorText = "La dirección de correo electrónico no es válida"
+
 export const safeConductValidator = yup.object().shape({
-  name: yup.string().required().min(2).max(55),
-  identityDocument: yup.string().required().min(3).max(55),
-  email: yup.string().email(),
+  name: yup
+    .string()
+    .required(requiredErrorText)
+    .max(55, ({ max }) => `Debe tener ${max} o menos caracteres`),
+  identityDocument: yup
+    .string()
+    .required(requiredErrorText)
+    .max(55, ({ max }) => `Debe tener ${max} o menos caracteres`),
+  email: yup
+    .string()
+    .email(emailErrorText)
+    .max(
+      emailMaxCharacters,
+      ({ max }) => `Debe tener ${max} o menos caracteres`
+    ),
 })
