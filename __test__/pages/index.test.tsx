121139d8a998ccb086bc0a7c2640b305d062ba33
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-03-18 13:09:46 +0100

    Fix success and fail message tests

--- __test__/pages/index.test.tsx
+++ __test__/pages/index.test.tsx
@@ -2,6 +2,7 @@ import userEvent from "@testing-library/user-event"
 import Home, {
   emailLabel,
   event,
+  failPdfGenerationMessage,
   identityDocumentLabel,
   nameLabel,
   submitButtonText,
@@ -41,28 +42,25 @@ describe("index", () => {
       expect(endDateElement).toBeDefined()
     })
     describe("form", () => {
-      let nameInput: HTMLElement,
-        identityDocumentInput: HTMLElement,
-        emailInput: HTMLElement,
-        submitButton: HTMLElement
-
       it("should have a name input", () => {
-        nameInput = screen.getByLabelText(nameLabel)
+        const nameInput = screen.getByLabelText(nameLabel)
 
         expect(nameInput).toBeVisible()
       })
       it("should have a identityDocument input", () => {
-        identityDocumentInput = screen.getByLabelText(identityDocumentLabel)
+        const identityDocumentInput = screen.getByLabelText(
+          identityDocumentLabel
+        )
 
         expect(identityDocumentInput).toBeVisible()
       })
       it.skip("should have an email input", () => {
-        emailInput = screen.getByLabelText(emailLabel)
+        const emailInput = screen.getByLabelText(emailLabel)
 
         expect(emailInput).toBeVisible()
       })
       it("should have a submit button", () => {
-        submitButton = screen.getByRole("button", {
+        const submitButton = screen.getByRole("button", {
           name: submitButtonText,
         })
 
@@ -70,17 +68,28 @@ describe("index", () => {
       })
 
       describe("submission handler", () => {
+        const fillFormAndSubmit = () => {
+          userEvent.type(screen.getByLabelText(nameLabel), "aaaaa")
+          userEvent.type(screen.getByLabelText(identityDocumentLabel), "aaaaa")
+          userEvent.click(screen.getByText(submitButtonText))
+        }
         it("should set a success message if response is ok", async () => {
           fetchMock.once("", { status: 200 })
 
-          userEvent.type(nameInput, "aaaaa")
-          userEvent.type(identityDocumentInput, "aaaaa")
-          userEvent.click(submitButton)
+          fillFormAndSubmit()
 
           const successMessage = await screen.findByText(
             successPdfGenerationMessage
           )
           expect(successMessage).toBeVisible()
+        })
+        it("should set an error message if response fails", async () => {
+          fetchMock.once("", { status: 500 })
+
+          fillFormAndSubmit()
+
+          const errorMessage = await screen.findByText(failPdfGenerationMessage)
+          expect(errorMessage).toBeVisible()
         })
       })
     })
