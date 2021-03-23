cb1af88879b6bb485daf61a493d373168c591c52
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-03-23 22:25:16 +0100

    Change pdf size

--- server/generatePdf.ts
+++ server/generatePdf.ts
@@ -2,7 +2,7 @@ import PDFDocument from "pdfkit"
 import { GeneratePdfData } from "utils/types"
 
 const generatePdf = (user: GeneratePdfData): Buffer | string => {
-  const pdf = new PDFDocument()
+  const pdf = new PDFDocument({ size: "A4" })
 
   pdf.text("Hello World")
   pdf.end()
