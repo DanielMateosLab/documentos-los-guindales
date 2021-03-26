4009031c562b28dbb1f88fbb9dafa9db8e350797
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-03-26 19:23:01 +0100

    Update the pdf

--- server/generatePdf.ts
+++ server/generatePdf.ts
@@ -1,13 +1,86 @@
+import moment from "moment"
 import PDFDocument from "pdfkit"
 import { GeneratePdfData } from "utils/types"
+import { formatDate } from "utils/utils"
+
+const title =
+  "Declaración responsable para facilitar la movilidad relacionada con actividades de formación"
+const datesText = "27 y 28 de marzo de 2021"
+const submissionDate = formatDate(moment(), { withYear: true })
+const indentation = 72 / 4
 
 const generatePdf = (user: GeneratePdfData): Buffer | string => {
-  const pdf = new PDFDocument({ size: "A4" })
+  const doc = new PDFDocument({
+    size: "A4",
+    info: {
+      Author: "África Rodríguez Nieves",
+      Title: title,
+    },
+  })
 
-  pdf.text("Hello World")
-  pdf.end()
+  doc.fontSize(16).text(title, { align: "center" }).moveDown(2)
 
-  return pdf.read()
+  doc.fontSize(12)
+
+  doc
+    .text(
+      "D.ª África Rodríguez Nieves, con DNI 31685789T, actuando como representante de la Asociación Cultural Los Guindales (CIF: G93505725)."
+    )
+    .moveDown(1)
+
+  doc
+    .text("Datos de contacto de la asociación:", {
+      indent: indentation,
+    })
+    .moveDown(1)
+
+  // @ts-ignore
+  doc
+    .list(
+      [
+        "Correo electrónico: guindaluc@yahoo.es",
+        "Domicilio: C/ Los Barrancos 2, Algatocín, Málaga.",
+        "Teléfono: 664 368 784.",
+      ],
+      { indent: indentation, bulletRadius: 2 }
+    )
+    .moveDown(1)
+
+  doc
+    .text("Declara responsablemente:")
+    .moveDown(1)
+
+    .text("Que D/D.ª ", { continued: true, indent: indentation })
+    .font("Helvetica-Bold")
+    .text(user.name, { continued: true })
+    .font("Helvetica")
+    .text(" con documento de identidad ", { continued: true })
+    .font("Helvetica-Bold")
+    .text(user.identityDocument, { continued: true })
+    .font("Helvetica")
+    .text(
+      ` es socio/a de la Asociación Cultural Los Guindales, en cuya sede realizará una formación los días ${datesText}`
+    )
+    .moveDown(1)
+  doc
+    .text(
+      "Para que conste a los efectos de facilitar los trayectos necesarios entre su lugar de residencia y el lugar de formación.",
+      {
+        indent: indentation,
+      }
+    )
+    .moveDown(2)
+
+  doc.text(`En Algatocín, a ${submissionDate}.`).moveDown(1)
+
+  doc.text("Fdo.: África Rodríguez Nieves", { align: "center" })
+  doc.image("public/firma_africa.jpg", 200, undefined, {
+    width: 200,
+  })
+
+  doc.end()
+
+  return doc.read()
 }
 
 export default generatePdf
