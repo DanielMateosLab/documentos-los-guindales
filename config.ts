bc6463032e82316672c1935629415a6a7d9d3ad5
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-06-06 15:34:02 +0200

    Create config.ts file to store configuration values

---
+++ config.ts
@@ -0,0 +1,9 @@
+export const sensibleData = {
+  REPRESENTATIVE: process.env.REPRESENTATIVE,
+  IDENTITY_DOCUMENT: process.env.IDENTITY_DOCUMENT,
+  ASSOCIATION: process.env.ASSOCIATION,
+  CIF: process.env.CIF,
+  EMAIL: process.env.EMAIL,
+  ADDRESS: process.env.ADDRESS,
+  PHONE_NUMBER: process.env.PHONE_NUMBER,
+}
